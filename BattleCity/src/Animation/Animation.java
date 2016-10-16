5e893f55aebe9f1df0689a4abede43fe5afab4ba
GuillermoSeewald <guilleseewald@gmail.com>
2016-10-15 23:21:04 -0300

    Animation
    Se crean las clases que representaran las diferentes animaciones

---
+++ BattleCity/src/Animation/Animation.java
@@ -0,0 +1,17 @@
+package Animation;
+
+import java.awt.Image;
+
+public abstract class Animation {
+	protected Image images[];
+	protected int posInAnimations;
+	protected int actualImage;
+	
+	public abstract void buildImages();
+	public void setPosInAnimations(int x){
+		posInAnimations=x;
+	}
+	public int getPosInAnimations(){
+		return posInAnimations;
+	}
+}
